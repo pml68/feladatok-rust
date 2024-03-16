63cc58b4342ebb2d5fff56169c4c352210ee3d76
pml68 <tutorialmester@gmail.com>
2024-03-16 11:54:13 +0100

    feat: complete the solution

--- kave-automata/src/main.rs
+++ kave-automata/src/main.rs
@@ -1,7 +1,6 @@
 use std::collections::hash_map::HashMap;
 use std::io::stdin;
 
-#[derive(Debug)]
 struct CoffeeMachine {
     item: String,
     extra: String,
@@ -18,7 +17,7 @@ impl<'a> CoffeeMachine {
     }
 
     fn list_items(&self, items: &'a HashMap<&'a str, u16>) {
-        println!("A következő italok közül választhat:");
+        println!("A következő italok közül választhatsz:");
         for (extra, price) in items {
             println!("{}: {}Ft", extra, price);
         }
@@ -106,29 +105,109 @@ impl<'a> CoffeeMachine {
         self.price += extras[extra.as_str()];
         self.extra = extra;
     }
+
+    fn pay_for_drink(&mut self, coins: &'a Vec<u8>) {
+        println!("A fizetendő összeg {}Ft", self.price);
+        let mut amount_paid: u16 = 0;
+        let mut accepted_coins_string = String::from("Dobj be egy érmét a következőek közül:");
+        for coin in coins {
+            accepted_coins_string = format!("{} {}Ft", accepted_coins_string, coin);
+        }
+        while self.price > amount_paid {
+            println!("{}", accepted_coins_string);
+            let mut coin_input = String::new();
+            stdin()
+                .read_line(&mut coin_input)
+                .expect("Nem lehet ennyire elszúrni");
+            let mut coin: u8 = coin_input.lines().next().unwrap().parse().unwrap();
+            while !coins.contains(&coin) {
+                println!("Kérlek, egy elfogadott érmét dobj be!");
+                coin_input = String::new();
+                stdin()
+                    .read_line(&mut coin_input)
+                    .expect("Nem lehet ennyire elszúrni");
+                coin = coin_input.lines().next().unwrap().parse().unwrap();
+            }
+
+            amount_paid += coin as u16;
+
+            if self.price > amount_paid {
+                println!("Eddig {} forintot fizettél ki.", amount_paid);
+            } else {
+                let mut change = amount_paid - self.price;
+                if change == 0 {
+                    println!(
+                        "Kifizetted a teljes összeget. Nincs visszajáró. Köszönjük a vásárlást!"
+                    );
+                } else {
+                    let mut change_coins = vec![0; coins.len()];
+                    for (index, coin) in coins.iter().enumerate() {
+                        change_coins[index] = (change - (change % *coin as u16)) / *coin as u16;
+                        change = change % *coin as u16;
+                    }
+                    let mut change_string =
+                        String::from("Kifizetted a teljes összeget. A visszajáród");
+                    for (index, coin) in change_coins.iter().enumerate() {
+                        if index != coins.len() - 1 {
+                            change_string =
+                                format!("{} {}x{}Ft,", change_string, coin, coins[index]);
+                        } else {
+                            change_string =
+                                format!("{} {}x{}Ft.", change_string, coin, coins[index]);
+                        }
+                    }
+
+                    println!("{}", change_string);
+                }
+            }
+        }
+    }
+
+    fn main(
+        &mut self,
+        items: &'a HashMap<&'a str, u16>,
+        extras: &'a HashMap<&'a str, u16>,
+        coins: &'a Vec<u8>,
+    ) {
+        self.list_items(items);
+        self.choose_item(items);
+        let allowed_extras = self.list_extras(extras);
+        self.choose_extra(extras, allowed_extras);
+        self.pay_for_drink(coins);
+    }
 }
 
 fn main() {
-    let mut coffeemachine1 = CoffeeMachine::new();
+    println!("Üdvözöllek. (V)ásárolsz, vagy (k)ilépsz? ");
+    let mut choice = String::new();
+    stdin()
+        .read_line(&mut choice)
+        .expect("Ennyire nem lehet elszúrni");
+    choice = choice.lines().next().unwrap().to_lowercase();
 
-    let mut items = HashMap::new();
-    items.insert("forró csoki", 300);
-    items.insert("tea", 200);
-    items.insert("kávé", 150);
+    while choice == "v" {
+        let mut items = HashMap::new();
+        items.insert("forró csoki", 300);
+        items.insert("tea", 200);
+        items.insert("kávé", 150);
 
-    let mut extras = HashMap::new();
-    extras.insert("tej", 50);
-    extras.insert("tejszín", 100);
-    extras.insert("cukor", 50);
-    extras.insert("citrom", 100);
+        let mut extras = HashMap::new();
+        extras.insert("tej", 50);
+        extras.insert("tejszín", 100);
+        extras.insert("cukor", 50);
+        extras.insert("citrom", 100);
 
-    coffeemachine1.list_items(&items);
+        let coins: Vec<u8> = vec![200, 100, 50, 20, 10];
 
-    coffeemachine1.choose_item(&items);
+        let mut coffeemachine = CoffeeMachine::new();
 
-    let allowed_extras = coffeemachine1.list_extras(&extras);
+        coffeemachine.main(&items, &extras, &coins);
 
-    coffeemachine1.choose_extra(&extras, allowed_extras);
-
-    println!("{:?}", coffeemachine1);
+        println!("Üdvözöllek. (V)ásárolsz, vagy (k)ilépsz? ");
+        choice = String::new();
+        stdin()
+            .read_line(&mut choice)
+            .expect("Ennyire nem lehet elszúrni");
+        choice = choice.lines().next().unwrap().to_lowercase();
+    }
 }
